50f49b5b6e5451b101b7d65393ba73cfe0bdc983
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-24 11:36:18 +0400

    alright I guess that's proper way

---
+++ src/main/java/net/snezhniy/Utils.java
@@ -0,0 +1,17 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package net.snezhniy;
+
+import java.util.Scanner;
+
+public class Utils {
+    /* Scanner factory */
+    public static Scanner makeScanner() {
+        var reader = System.console().reader();
+        return new Scanner(reader);
+    }
+}
