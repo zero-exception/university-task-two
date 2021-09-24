f336cfa94632ff6522e76162c9109480779414bc
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-24 12:39:09 +0400

    last one

---
+++ src/main/java/net/snezhniy/solutions/Solution10.java
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package net.snezhniy.solutions;
+
+import net.snezhniy.Utils;
+import net.snezhniy.interfaces.Solution;
+
+import java.util.stream.Stream;
+
+/*
+№10. Ввести строку, состоящую из нескольких слов, разделенных пробелами.
+Подсчитать количество символов в строке (исключая пробелы).
+*/
+
+public class Solution10 implements Solution {
+    @Override
+    public void run() {
+        var scan = Utils.makeScanner();
+
+        System.out.print("Введите строку: ");
+        var input = scan.nextLine();
+
+        var length = input.replaceAll("\\s+", "").length();
+        System.out.printf("Длина строки без пробелов: %s", length);
+    }
+}
