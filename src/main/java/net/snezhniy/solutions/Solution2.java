bca33b0e66b6a74c4abb7fd4bae8fc8df5a0a046
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-24 11:40:20 +0400

    second solution

---
+++ src/main/java/net/snezhniy/solutions/Solution2.java
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package net.snezhniy.solutions;
+
+import net.snezhniy.Utils;
+import net.snezhniy.interfaces.Solution;
+
+/*
+#2 Ввести строку, состоящую из нескольких слов, разделенных пробелами.
+Подсчитать количество слов в строке.
+*/
+
+public class Solution2 implements Solution {
+    @Override
+    public void run() {
+        var scan = Utils.makeScanner();
+
+        System.out.print("Введите слово: ");
+        var input = scan.nextLine().strip();
+        var wordsCount = input.split("\\s+").length;
+
+        System.out.printf("Количество слов: %s\n", wordsCount);
+    }
+}
