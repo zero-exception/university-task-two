01b8cc7bdc8385ad96d642179d2e7742e015be81
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-24 10:24:40 +0400

    first solution and it works

---
+++ src/main/java/net/snezhniy/solutions/Solution1.java
@@ -0,0 +1,38 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package net.snezhniy.solutions;
+
+import net.snezhniy.interfaces.Solution;
+
+import java.util.Scanner;
+
+/*
+Ввести строку, состоящую из нескольких слов, разделенных пробелами.
+Ввести произвольное слово. Найти его в строке. Если слово найдено, выдать на
+консоль позицию строки, с которой это слово начинается. Если такого слова
+нет, выдать сообщение об этом на консоль.
+*/
+
+public class Solution1 implements Solution {
+    @Override
+    public void run() {
+        var scan = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        var str = scan.nextLine().strip();
+
+        System.out.print("Введите слово: ");
+        var search = scan.nextLine().strip();
+
+        var indexOfSearch = str.indexOf(search);
+        if (indexOfSearch < 0) {
+            System.out.println("Указанное слово не найдено в указанной строке.");
+        } else {
+            System.out.printf("Индекс слова в строке: %s", indexOfSearch);
+        }
+    }
+}
